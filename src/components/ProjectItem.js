6e999503ee2400038ca6c9246eab9659ef72bfee
Mwenje <mwenje24@gmail.com>
2022-07-15 11:10:24 +0300

    passed projectItem component to the projectListwith destructured data from the user component

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const projectItems = technologies.map((listvalue) => {
+    return <span key={listvalue}>{listvalue}</span>;
+  });
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        <div>{projectItems}</div>
       </div>
     </div>
   );
